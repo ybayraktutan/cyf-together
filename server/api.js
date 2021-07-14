e85c16a0877281229039abc0bb7982225aeb7189
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-07-14 13:24:57 +0100

    adding register endpoint

--- server/api.js
+++ server/api.js
@@ -1,9 +1,41 @@
 import { Router } from "express";
-
+import { pool } from "./db";
+const uuid=require("uuid");
 const router = new Router();
 
 router.get("/", (_, res) => {
-	res.json({ message: "Hello, world!" });
+ pool.query("SELECT * FROM users", (error, result) => {
+		res.json(result.rows);
+ });
+});
+
+router.post("/register", (req, res) => {
+const { firstname,email,password }=req.body;
+const newUser = {
+	id: uuid.v4(),
+	firstname:firstname,
+	email:email,
+	password:password,
+};
+ pool.query(
+		"SELECT * FROM users where email=$1",
+		[newUser.email],
+		(error, result) => {
+			if (result.rows.length>0){
+res.send("error....already have that email");
+			} else{
+pool.query(
+	"INSERT INTO users (id,firstname,email,password) VALUES ($1,$2,$3,$4)",
+	[newUser.id, newUser.firstname, newUser.email, newUser.password],
+	(error, result) => {
+		res.send("user is added");
+	}
+);
+	}
+	}
+ );
 });
 
 export default router;
+
+
