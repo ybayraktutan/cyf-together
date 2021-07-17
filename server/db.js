f133bb3dd18bea19d5b4aff773826478b9f5662f
ybayraktutan <74928622+ybayraktutan@users.noreply.github.com>
2021-07-17 14:15:56 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 const dbUrl =
 	process.env.DATABASE_URL ||
-	"postgres://Administrator:yasemin@@localhost:5432/cyf";
+	"postgres://codeyourfuture:codeyourfuture@localhost:5432/cyf";
 
 export const pool = new Pool({
 	connectionString: dbUrl,
