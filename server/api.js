c52beb965c61bf6e3ee9b7fd9fe3818ea1e06699
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-07-26 20:15:03 +0100

    working on looping-practise

--- server/api.js
+++ server/api.js
@@ -145,29 +145,41 @@ function authenticateToken(req, res, next) {
 	});
 }
 router.get("/practise", authenticateToken, (req, res) => {
-	console.log("hello from practise");
 	const userID = req.user.userid;
 	const currentTime = new Date();
 	pool
 		.query("SELECT lastpractise_id FROM users WHERE id=$1", [userID])
 		.then((result) => {
-			console.log(currentTime);
-			console.log(result.rows[0].lastpractise_time);
-			console.log(currentTime - result.rows[0].lastpractise_time);
-			if (currentTime - result.rows[0].lastpractise_time < 0) {
-				res.send("Please visit later for new practise");
-			}
-			console.log(result.rows[0].lastpractise_id);
-			pool
-				.query("SELECT * FROM practises WHERE id=$1", [
-					result.rows[0].lastpractise_id + 1,
-				])
+			let user_last_practice_id = result.rows[0].lastpractise_id;
+				pool
+				.query("SELECT id FROM practises ORDER BY id DESC LIMIT 1")
 				.then((result) => {
-					return res.json(result.rows);
+						let last_practise_in_db = result.rows[0].id;
+						console.log("last pra"+user_last_practice_id);
+						console.log("last pra" + last_practise_in_db);
+
+						if (user_last_practice_id < last_practise_in_db) {
+							console.log("in if");
+							pool.query("SELECT * FROM practises WHERE id=$1", [
+							result.rows[0].lastpractise_id + 1])
+							.then((result) => {
+							return res.json(result.rows);
 				})
 				.catch((e) => res.send(JSON.stringify(e)));
-		})
-		.catch((e) => res.send(JSON.stringify(e)));
+
+						}
+				return res.json(result.rows[0].id);
+				});
+			console.log(currentTime - result.rows[0].lastpractise_time);
+			// if (currentTime - result.rows[0].lastpractise_time < 0) {
+			// 	res.send("Please visit later for new practise");
+			// }
+			console.log(result.rows[0].lastpractise_id);
+});
+
+
+
+
 });
 
 router.post("/reflects", authenticateToken, (req, res) => {
@@ -178,7 +190,7 @@ router.post("/reflects", authenticateToken, (req, res) => {
 	console.log(req.body);
 	pool
 		.query(
-			"INSERT INTO reflects (id,user_id,answer,practice_id) VALUES (DEFAULT,$1,$2,$3)",
+			"INSERT INTO reflects (user_id,answer,practice_id) VALUES ($1,$2,$3)",
 			[userID, answer, practice_id]
 		)
 		.then((result) => {
@@ -194,4 +206,5 @@ router.post("/reflects", authenticateToken, (req, res) => {
 		})
 		.catch((e) => res.send(JSON.stringify(e)));
 });
+
 export default router;
