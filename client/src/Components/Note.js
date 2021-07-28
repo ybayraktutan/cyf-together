6832bec89c62ee2e8515c00c04fb51354b5bc40f
jmodiale <65014009+jmodiale@users.noreply.github.com>
2021-07-28 08:50:00 +0100

    modify

--- client/src/Components/Note.js
+++ client/src/Components/Note.js
@@ -49,22 +49,23 @@ const Note = ({ data }) => {
 									Today&apos;s Practice
 								</span>
 							</div>
-							</div>
-						</Card.Body>
-						<Card.Body>
-							<Form.Group
-								className="mb-3"
-								controlId="exampleForm.ControlTextarea1"
-							>
-								<Form.Control
-									as="textarea"
-									size="sm"
-									value={value}
-									onChange={(e) => setValue(e.target.value)}
-									style={({ fontSize: "8px", height: "250px" })}
-								/>
-							</Form.Group>
-						</Card.Body>
+						</div>
+					</Card.Body>
+					<Card.Body>
+						<Form.Group
+							className="mb-3"
+							controlId="exampleForm.ControlTextarea1"
+						>
+							<Form.Control
+								as="textarea"
+								size="sm"
+								value={value}
+								onChange={(e) => setValue(e.target.value)}
+								style={{ fontSize: "8px", height: "250px" }}
+							/>
+						</Form.Group>
+					</Card.Body>
+					<Card.Body>
 						<div className="btn-note">
 							<Link to="/confirmation" onClick={complete}>
 								<Button
