4fee8e22c3eadef857919653652f1ee02bac68ff
ybayraktutan <74928622+ybayraktutan@users.noreply.github.com>
2021-07-29 11:12:52 +0100

    confirmation page CSS completed, media queery added to practice

--- client/src/Pages/Confirmation.js
+++ client/src/Pages/Confirmation.js
@@ -1,87 +1,61 @@
 /*eslint linebreak-style: ["error", "windows"]*/
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container, Button, Card, Col, Row } from "react-bootstrap";
+import { Container, Button, Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Icon } from "@iconify/react";
-import shareIcon from "@iconify-icons/feather/share";
-import messageCircle from "@iconify-icons/feather/message-circle";
 import { Emoji } from "emoji-mart";
 import "../Style/Confirmation.css";
 
 const Confirmation = () => {
-	const linkStyle = {
-		width: "22rem",
-		height: "32rem",
-		borderRadius: "20px",
-	};
-
-	const iconStyle = {
-		fontWeight: "bold",
-		fontSize: "18px",
-	};
-
+	
 	return (
-		<div id="confirm">
-			<Container style={linkStyle} className="container">
-				<Card className="mb-3" style={linkStyle}>
-					<Card.Body className="confirm-top">
-						<div className="top-contents">
-							<div>
-								<p style={{ fontSize: "11px" }}> Great job </p>
-								<h5>Practice complete!</h5>
-							</div>
-							<div>
-								<Emoji
-									set={"apple"}
-									emoji={"star-struck"}
-									size={50}
-									fallback={(emoji, props) => {
-										return emoji ? `:${emoji.short_names[0]}:` : props.emoji;
-									}}
-								/>
-							</div>
-						</div>
-					</Card.Body>
-					<Card.Body className="confirm-bottom">
-						<div>
-							<Row id="share-icons">
-								<Col>
-									<Icon icon={shareIcon} style={iconStyle} />
-									<p>Share practice</p>
-								</Col>
-								<Col>
-									<Icon icon={messageCircle} style={iconStyle} />
-									<p>Share your answer</p>
-								</Col>
-							</Row>
-						</div>
-						<div className="btn-confirm">
-							<Link to="#">
-								<Button
-									type="button"
-									variant="primary"
-									size="sm"
-									style={{
-										color: "white",
-										backgroundColor: "#7DC579",
-										width: "100%",
-										borderRadius: "7px",
-										outline: "none",
-										borderStyle: "none",
-										borderColor: "none",
-									}}
-								>
-									{" "}
-									Continue
-								</Button>
-							</Link>
-						</div>
-					</Card.Body>
-				</Card>
-			</Container>
-		</div>
+		<Container
+			fluid
+			className="confirmation-container"
+			style={{ margin: "0", padding: "0" }}
+		>
+			<Card
+				className="confirmation-top"
+				variant="default"
+				style={{ backgroundColor: "#6360FF", border: "none" }}
+			>
+				<Card.Body>
+					<Card.Title as="h6">Great job</Card.Title>
+					<Card.Title as="h2">Practice complete!</Card.Title>
+					<Emoji
+						set={"apple"}
+						emoji={"star-struck"}
+						size={150}
+						fallback={(emoji, props) => {
+							return emoji ? `:${emoji.short_names[0]}:` : props.emoji;
+						}}
+					/>
+				</Card.Body>
+			</Card>
+			<Card
+				className="confirmation-bottom"
+				style={{ backgroundColor: "#F1F1FA", margin: "0", padding: "0" }}
+			>
+				<Link to="/home">
+					<Button
+						className="btn-continue"
+						type="button"
+						variant="default"
+						style={{
+							color: "#FFF",
+							backgroundColor: "#7DC579",
+							padding: "30px",
+							fontSize: "4vw",
+							borderRadius: "20px",
+						}}
+					>
+						Continue
+					</Button>
+				</Link>
+			</Card>
+		</Container>
 	);
 };
+
 
 export default Confirmation;
